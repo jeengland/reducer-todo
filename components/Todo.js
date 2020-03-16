223942968c64a183fa2462b03cc2e5c125a0d5e5
unknown <jacob.e.england@gmail.com>
2020-03-16 14:17:42 -0500

    Updated completed time to better indicate support

--- components/Todo.js
+++ components/Todo.js
@@ -6,7 +6,7 @@ import moment from 'moment';
 const Todo = (props) => {
     return (
     <li onClick={() => props.dispatch({ type: 'TOGGLE', id: props.todo.id })}>
-        <span><p className={props.todo.completed ? 'completed' : undefined }>{props.todo.item}</p>&nbsp;<time className='time'>{props.todo.time ? props.todo.time.fromNow() : "   " }</time></span>
+        <span><p className={props.todo.completed ? 'completed' : undefined }>{props.todo.item}</p>&nbsp;<time className='time'>{props.todo.time ? 'Completed ' + props.todo.time.format('MMMM Do YYYY, h:mm:ss a') : "   " }</time></span>
     </li>
     
     )
